09a6f0359d2692d9208571519493878b9fe72d9b
Technowolf-01 <mrnithinneo@gmail.com>
2020-12-19 21:04:03 +0530

    Fibonacci code

---
+++ Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fib = new fibo();
+            fib.calc();
+        }
+    }
+}
---
+++ Fibonacci/Fibonacci/fibo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class fibo
+    {
+        public void calc()
+        {
+            int i,n, num1=0, num2=1, temp;
+            Console.WriteLine("enter the input");
+            n = Convert.ToInt32(Console.ReadLine());
+            for(i = 0; i < n; i++)
+            {
+                
+                Console.WriteLine(num1);
+                temp = num1 + num2;
+                num1 = num2;
+                num2 = temp;
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
