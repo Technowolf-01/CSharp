1d59b9b6153aac8b74d9f55069f893ea48668b4b
Technowolf-01 <mrnithinneo@gmail.com>
2020-12-19 21:13:24 +0530

    Changed variable name

--- Fibonacci/Fibonacci/fibo.cs
+++ Fibonacci/Fibonacci/fibo.cs
@@ -10,16 +10,16 @@ namespace Fibonacci
     {
         public void calc()
         {
-            int i,n, num1=0, num2=1, temp;
+            int i,n, num1=0, num2=1, num3;
             Console.WriteLine("enter the input");
             n = Convert.ToInt32(Console.ReadLine());
             for(i = 0; i < n; i++)
             {
                 
                 Console.WriteLine(num1);
-                temp = num1 + num2;
+                num3 = num1 + num2;
                 num1 = num2;
-                num2 = temp;
+                num2 = num3;
                 
             }
             Console.ReadLine();
