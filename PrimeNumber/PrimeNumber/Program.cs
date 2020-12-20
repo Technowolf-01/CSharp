8a96e8375ae1bbd0fa1c169ae3fc88527f16defc
Technowolf-01 <mrnithinneo@gmail.com>
2020-12-20 13:23:56 +0530

    Prime Number

---
+++ PrimeNumber/PrimeNumber/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, i , m = 0, flag = 0 ;
+            Console.WriteLine("enter the number");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            m = num / 2;
+            for(i=2;i<=m;i++)
+            {
+                if(num%i==0)
+                {
+                    Console.WriteLine("Not a prime number");
+                    flag = 1;
+                    break;
+                }
+            }
+            if(flag==0)
+            {
+                Console.WriteLine("Prime Number");
+            }
+            Console.Read();
+
+        }
+
+
+    }
+}
