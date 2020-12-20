7d0d2ba891ff9f260b408b59e67d1cfcf8288bdb
Technowolf-01 <mrnithinneo@gmail.com>
2020-12-20 14:42:55 +0530

    Factorial

---
+++ Factorial/Factorial/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, Fac;
+            Console.WriteLine("enter the number");
+            num = Convert.ToInt32(Console.ReadLine());
+            Fac = num;
+            while(num>1)
+            {
+                Fac = Fac * (num - 1);
+                --num;
+            }
+            Console.WriteLine(Fac);
+            Console.ReadLine();
+        }
+    }
+}
